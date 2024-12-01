0670d56283501d09038061953c8ccd0d47ca37a9
firstlast-0 <127908930+firstlast-0@users.noreply.github.com>
2024-12-01 20:14:40 +0800

    Add indicator for current route

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,12 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect, useRef } from "react";
 
 function Nav() {
+    let location = useLocation();
+    const aboutRef = useRef(null);
+    const projectsRef = useRef(null);
+    const skillsRef = useRef(null);
+
+    useEffect(() => {
+        if (location.pathname === '/') {
+            aboutRef.current.className = 'selected';
+        } else if (location.pathname === '/projects') {
+            projectsRef.current.className = 'selected';
+        } else {
+            skillsRef.current.className = 'selected';
+        }
+    }, [location]);
+
     return (
         <nav>
             <ul>
-                <li><Link to="/"><span className="initial">A</span>BOUT</Link></li>
-                <li><Link to="/projects"><span className="initial">P</span>ROJECTS</Link></li>
-                <li><Link to="/skills"><span className="initial">S</span>KILLS</Link></li>
+                <li ref={aboutRef}><Link to="/"><span className="initial">A</span>BOUT</Link></li>
+                <li ref={projectsRef}><Link to="/projects"><span className="initial">P</span>ROJECTS</Link></li>
+                <li ref={skillsRef}><Link to="/skills"><span className="initial">S</span>KILLS</Link></li>
             </ul>
         </nav>
     );
